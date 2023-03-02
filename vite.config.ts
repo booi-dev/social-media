01736bc9d8f50b6a33490d26563c9843c35bbb96
Booi <56810550+booimangang@users.noreply.github.com>
2023-03-02 23:28:42 +0530

    Configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    // environment: "jsdom",
+    // setupFiles: "@testing-library/jest-dom",
+    // css: true,
+  },
   base: "/social-media-clone/",
 });
