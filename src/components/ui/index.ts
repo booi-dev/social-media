54a79e6cb15f42e398ae7d629fb4cb749f39529f
Booi <56810550+booimangang@users.noreply.github.com>
2023-03-28 10:59:14 +0530

    Create index for UI

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { default as AppIcon } from "./AppIcon";
+export { default as BackDrop } from "./BackDrop";
+export { default as VerificationBadge } from "./VerificationBadge";
+export { default as WarningMsg } from "./WarningMsg";
+export { default as AutoResizeTextArea } from "./AutoResizeTextArea";
+export { default as RichEditor } from "./RichEditor";
