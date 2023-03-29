6e8b0f848665df11aa942008853e25fd73e18227
Booi <56810550+booimangang@users.noreply.github.com>
2023-03-29 20:39:46 +0530

    Add firebase and create config file

---
+++ firebase.ts
@@ -0,0 +1,28 @@
+import firebase from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export default app;
+
+// signInWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//   });
