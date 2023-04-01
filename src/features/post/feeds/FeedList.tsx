0ad3b38e6440c08e472e72a8aef81d1e06d34633
Booi <56810550+booimangang@users.noreply.github.com>
2023-04-01 23:30:35 +0530

    Refactor feed list

--- src/features/post/feeds/FeedList.tsx
+++ src/features/post/feeds/FeedList.tsx
@@ -16,41 +16,30 @@ function FeedList() {
 
   const sortedPosts = useMemo(() => sortArray(posts), [posts]);
 
-  return (
-    <div className="bg-inherit">
-      {sortedPosts.map((p: PostType) => {
-        if (p.postType?.type === "reply" && p.postType.originalPostId) {
-          return (
-            <div
-              key={p.pid}
-              className="border-x-[1px] border-b-[1px] border-app-white-5  dark:border-app-gray-1"
-            >
-              <ReplyPostFeed
-                post={p}
-                typeState={{
-                  type: "reply",
-                  originalPostId: p.postType.originalPostId,
-                }}
-              />
-            </div>
-          );
-        }
+  return sortedPosts.map((p: PostType) => (
+    <div
+      key={p.pid}
+      className="border-x-[1px] border-b-[1px] border-app-white-5  dark:border-app-gray-1"
+    >
+      {p.postType.type === "normal" && (
+        <Feed
+          post={p}
+          typeState={{ type: "normal" }}
+          actionsPanel={ActionsPanel}
+        />
+      )}
 
-        return (
-          <div
-            key={p.pid}
-            className="border-x-[1px] border-b-[1px] border-app-white-5  dark:border-app-gray-1"
-          >
-            <Feed
-              post={p}
-              typeState={{ type: "normal" }}
-              actionsPanel={ActionsPanel}
-            />
-          </div>
-        );
-      })}
+      {p.postType?.type === "reply" && p.postType.originalPostId && (
+        <ReplyPostFeed
+          post={p}
+          typeState={{
+            type: "reply",
+            originalPostId: p.postType.originalPostId,
+          }}
+        />
+      )}
     </div>
-  );
+  ));
 }
 
 export default FeedList;
