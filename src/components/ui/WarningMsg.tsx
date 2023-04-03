b7564c5f8796a8a33a70c8b224af8ea702a0396e
Booi <56810550+booimangang@users.noreply.github.com>
2023-04-04 01:47:04 +0530

    Update warning msg component

--- src/components/ui/WarningMsg.tsx
+++ src/components/ui/WarningMsg.tsx
@@ -30,9 +30,11 @@ function WarningMsg(props: WarningMsgType) {
 
   return (
     <>
-      <div className="fixed top-1/2 left-1/2 z-20 h-auto w-[360px] -translate-x-2/4 -translate-y-1/2 rounded-sm bg-inherit p-8 text-inherit shadow-lg">
+      <div className="fixed top-1/2 left-1/2 z-20 h-auto w-full -translate-x-2/4 -translate-y-1/2 rounded-sm bg-inherit p-8 text-inherit shadow-lg sm:w-[380px]">
         <h1 className="text-app-font-20 font-bold"> {warningText}</h1>
-        <p className="text-app-font-15 leading-4 text-inherit">{warningDesc}</p>
+        <p className="py-2 text-app-font-15 leading-4 text-inherit">
+          {warningDesc}
+        </p>
         <div className="mt-4 [&>button]:block [&>button]:w-full [&>button]:rounded-md [&>button]:p-2  [&>button]:font-bold [&>button]:transition-all [&>button]:duration-500">
           <button
             type="button"
