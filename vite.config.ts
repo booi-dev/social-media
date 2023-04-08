ad6482a0297e91923f85d80618d7f7c0e7a249a3
Booi <56810550+booimangang@users.noreply.github.com>
2023-04-08 22:20:52 +0530

    Refactor

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    // environment: "jsdom",
-    // setupFiles: "@testing-library/jest-dom",
-    // css: true,
   },
   base: "/social-media/",
 });
